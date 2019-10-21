c96f79ea61f26cdf14c7c4273eeeb7129190c2c4
Luccas Fraga <luccasfraga@Luccas-4.local>
2019-10-21 11:42:50 -0300

    initial settings

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor() {
+    this.serve = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.serve.use(express.json());
+  }
+
+  routes() {
+    this.serve.use(routes);
+  }
+}
+
+export default new App().serve;
